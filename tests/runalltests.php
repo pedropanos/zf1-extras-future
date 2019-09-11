436c198a556f5030a58eae8879a38633eb53c33f
George Appleton <georgea@intelligentpositioning.com>
2019-09-11 15:55:08 +0100

    updates readme, composer.json and fixes a depricated each function

--- tests/runalltests.php
+++ tests/runalltests.php
@@ -55,11 +55,11 @@ sort($files);
 $result = 0;
 
 // run through phpunit
-while(list(, $file)=each($files)) {
+foreach ($files as $file) {
     echo "Executing {$file}" . PHP_EOL;
     system($PHPUNIT . ' --stderr -d memory_limit=-1 -d error_reporting=E_ALL\&E_STRICT -d display_errors=1 ' . escapeshellarg($file), $c_result);
     echo PHP_EOL;
-    
+
     if ($c_result) {
         $result = $c_result;
     }
